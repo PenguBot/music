3b78579da9ea42dcac689068ac53e95a1c05b36b
Jacz <itsmejacz@gmail.com>
2020-06-01 11:47:51 +1000

    Remove files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Client as KlasaClient } from "klasa";
-import { MusicClient as Client } from "./lib/Client";
-
-export { Client };
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-module.exports[KlasaClient.plugin] = Client[KlasaClient.plugin];
