5903c4e0ff508c2f4efedd5750da39270167d3ef
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-08-13 04:08:39 +0530

    fix: spotify playlist regex

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,5 +1,5 @@
 export const WILDCARD = /(?:scsearch:|ytsearch:).*/i;
 export const DUMP = /https:\/\/paste.pengubot.com\/(.*)/i;
-export const SPOTIFY_PLAYLIST = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:playlist\/|user\/spotify\/playlist\/|\?uri=spotify:playlist:)([1-z]{22})/i;
+export const SPOTIFY_PLAYLIST = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:playlist\/|user\/spotify\/playlist\/|\?uri=spotify:playlist:)([0-9a-z]{22})/i;
 export const SPOTIFY_ALBUM = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:album\/|\?uri=spotify:album:)((\w|-){22})/i;
 export const SPOTIFY_TRACK = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:track\/|\?uri=spotify:track:)((\w|-){22})/i;
